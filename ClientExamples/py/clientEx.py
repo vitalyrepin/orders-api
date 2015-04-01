5b4aef2d00ed56a3488e997d13392c25b657f859
Vitaly Repin <vitaly.repin@gmail.com>
2015-04-01 16:45:02 +0300

    Python example: AuthTokExpired is handled

--- ClientExamples/py/clientEx.py
+++ ClientExamples/py/clientEx.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 
+'''
+  Copyright 2015 Metida Print Ab Oy
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+  http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied. See the License for the
+  specific language governing permissions and limitations
+  under the License.
+'''
+
 import sys
 sys.path.append('../../gen-py')
+import time
 
 from Metida import OrderManager
 from Metida.ttypes import *
@@ -20,7 +37,7 @@ try:
 
 #  transport = TSSLSocket.TSSLSocket('orders.metidaprint.com', 443, True, 'cacert.pem')
 
-  transport = TSocket.TSocket('localhost', 30303)
+#  transport = TSocket.TSocket('localhost', 30303)
 
   # Buffering is critical. Raw sockets are very slow
   transport = TTransport.TBufferedTransport(transport)
@@ -113,7 +130,14 @@ try:
     ordId = client.newOrder(authToken, shipment, [product], misc)
   except OrderError as err:
     print '[OK] Error: ' + str(err.code) + ' ' + err._message
-  
+ 
+  # Testing error case: AuthTokenExpired
+  print 'Sleeping for 30 seconds...';
+  time.sleep(30)
+  try:
+    client.ping(authToken)
+  except AuthTokenExpired as err:
+    print '[OK] Error: ' + err._message
 
   transport.close()
 
